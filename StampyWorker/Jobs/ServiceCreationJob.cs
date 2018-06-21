175996c669782ac8d08a7a46d1a87ff0fb433c66
Hawk Foreste <hawfor@microsoft.com>
2018-06-21 11:58:49 -0700

    Fix parameters set for antares deployment exe

--- StampyWorker/Jobs/ServiceCreationJob.cs
+++ StampyWorker/Jobs/ServiceCreationJob.cs
@@ -20,6 +20,7 @@ namespace StampyWorker.Jobs
         {
             _logger = logger;
             _result = new JobResult();
+            _result.Status = StampyCommon.Models.JobResult.None;
             _parameters = cloudStampyArgs;
             _statusMessageBuilder = new StringBuilder();
         }
@@ -28,12 +29,14 @@ namespace StampyWorker.Jobs
         {
             if (!File.Exists(AntaresDeploymentExecutablePath))
             {
-                _logger.WriteError(_parameters, "Cannot find file", new FileNotFoundException("Cannot find file", Path.GetFileName(AntaresDeploymentExecutablePath)));
+                var ex = new FileNotFoundException("Cannot find file", Path.GetFileName(AntaresDeploymentExecutablePath));
+                _logger.WriteError(_parameters, "Cannot find file", ex);
+                throw ex;
             }
 
             var processStartInfo = new ProcessStartInfo();
             processStartInfo.FileName = AntaresDeploymentExecutablePath;
-            processStartInfo.Arguments = $"SetupPrivateStampWithGeo {_parameters.CloudName} /SubscriptionId=b27cf603-5c35-4451-a33a-abba1a08c9c2 /VirtualDedicated:true /bvtCapableStamp:true /DefaultLocation:\"Central US\"";
+            processStartInfo.Arguments = $"SetupPrivateStampWithGeo {_parameters.CloudName} /SubscriptionId:b27cf603-5c35-4451-a33a-abba1a08c9c2 /VirtualDedicated:true /bvtCapableStamp:true /DefaultLocation:\"Central US\"";
             processStartInfo.UseShellExecute = false;
             processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             processStartInfo.RedirectStandardError = true;
@@ -51,6 +54,7 @@ namespace StampyWorker.Jobs
             }
 
             _result.Message = _statusMessageBuilder.ToString();
+            _result.Status = StampyCommon.Models.JobResult.Passed;
             return Task.FromResult(_result);
         }
 
@@ -58,10 +62,10 @@ namespace StampyWorker.Jobs
         {
             if (!string.IsNullOrWhiteSpace(e.Data))
             {
-                if (e.Data.Contains("Total Time for Template"))
+                if (e.Data.Contains("<ERROR>") || e.Data.Contains("<Exception>") || e.Data.Contains("Error:"))
                 {
                     _statusMessageBuilder.AppendLine(e.Data);
-                    _result.Status = StampyCommon.Models.JobResult.Passed;
+                    _result.Status = StampyCommon.Models.JobResult.Failed;
                 }
             }
         }
