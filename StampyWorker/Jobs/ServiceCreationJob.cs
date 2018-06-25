15548d34c054b372ff24bc071962334b33946365
Hawk Foreste <hawfor@microsoft.com>
2018-06-25 09:27:08 -0700

    Fix deployment failures due to firewall rules

--- StampyWorker/Jobs/ServiceCreationJob.cs
+++ StampyWorker/Jobs/ServiceCreationJob.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace StampyWorker.Jobs
 {
@@ -53,6 +54,17 @@ namespace StampyWorker.Jobs
                 createProcess.WaitForExit();
             }
 
+            var definitionsPath = $@"\\AntaresDeployment\PublicLockBox\{_parameters.CloudName}\developer.definitions";
+            var cloudStampyFirewallRules =
+@"
+<redefine name=""SqlFirewallAddressRangesList"" value=""131.107.0.0 / 16; 167.220.0.0 / 16; 65.55.188.0 / 24"" /><redefine name=""FirewallLockdownWhitelistedSources"" value=""131.107.0.0 / 16; 167.220.0.0 / 16; 65.55.188.0 / 24"" />
+";
+            if (!TryModifyDefinitions(definitionsPath, cloudStampyFirewallRules))
+            {
+                _statusMessageBuilder.AppendLine("Failed to add cloud stampy firewall rules to developer.definitions file.");
+                _result.JobStatus = Status.Failed;
+            }
+
             _result.Message = _statusMessageBuilder.ToString();
             _result.JobStatus = _result.JobStatus == Status.None ? Status.Passed : _result.JobStatus;
             return Task.FromResult(_result);
@@ -86,6 +98,73 @@ namespace StampyWorker.Jobs
             {
                 return Path.Combine(_parameters.BuildPath, @"hosting\Azure\RDTools\Tools\Antares\AntaresDeployment.exe");
             }
+        }
+
+        private bool TryModifyDefinitions(string definitionPath, string additionalDefinitions)
+        {
+
+            try
+            {
+                string xmlTemplate = (@"<?xml version=""1.0"" encoding=""utf-8""?>
+<definitions xmlns=""http://schemas.microsoft.com/configdefinitions/"">
+<import path=""private.definitions"" />
+{0}</definitions>");
+
+                var modifyXML = string.Format(xmlTemplate, additionalDefinitions);
+
+                var xmlreader2 = XmlReader.Create(new StringReader(modifyXML));
+                var origianlDoc = new XmlDocument();
+                origianlDoc.Load(definitionPath);
+                var modifiedDoc = new XmlDocument();
+                modifiedDoc.Load(xmlreader2);
+
+                var original = GetHashTable(origianlDoc);
+                var modified = GetHashTable(modifiedDoc);
+
+                Merge(original, modified);
+
+                var xml = "";
+                foreach (var name in original.Keys)
+                {
+                    xml += string.Format(@"<redefine name=""{0}"" value=""{1}"" />", name, original[name]);
+                    xml += string.Format("\r\n");
+                }
+
+                var text = string.Format(xmlTemplate, xml);
+                File.WriteAllText(definitionPath, text);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteError(_parameters, "Failed to modify definitions file", ex);
+                return false;
+            }
+
+
+        }
+
+        private static void Merge<TKey, TValue>(IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second)
+        {
+            if (second == null || first == null) return;
+            foreach (var item in second)
+                if (!first.ContainsKey(item.Key))
+                    first.Add(item.Key, item.Value);
+                else
+                    first[item.Key] = item.Value;
+        }
+
+        private Dictionary<string, string> GetHashTable(XmlDocument doc)
+        {
+            var table = new Dictionary<string, string>();
+            XmlNodeList scenarios = doc.GetElementsByTagName("redefine");
+            foreach (XmlLinkedNode scenarioNode in scenarios)
+            {
+                var name = scenarioNode.Attributes["name"].Value;
+                var value = scenarioNode.Attributes["value"].Value;
+                if (!table.ContainsKey(name))
+                    table.Add(name, value);
+            }
+            return table;
         }
 
     }
